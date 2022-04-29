0991649de11e07bfcde2c1d465a2f134b974450a
cdmr <carlosd.marceleno@gmail.com>
2022-04-29 12:52:16 -0500

    restart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    CarouselComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
